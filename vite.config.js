2f3ad375731c37d317a7e65c6605d9e296b2a0df
Simon Wood <swood@logican.co.uk>
2024-02-05 15:28:39 +0700

    Moved build output to docs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+  },
   define: {
     // enable hydration mismatch details in production build
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
